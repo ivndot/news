98fb6b58bcc6aba84369205b3b70f07029ca311e
Ivan <ivan_alamos@outlook.com>
2020-08-21 22:43:29 -0500

    -- THIRD COMMIT -- -RESPONSIVE 1 YES -RESPONSIVE 2 YES -ANIMATION    NO

---
+++ main.js
@@ -0,0 +1,27 @@
+
+/* 
+    AÑADIMOS UN LISTENER AL BOTON BTN MENU
+*/
+
+var state=false;
+
+document.querySelector(".btn-menu").addEventListener("click", () => {
+
+    if(state){
+        state=false;
+        closeNav();
+    }else{
+        state=true;
+        openNav();
+    }
+});
+
+
+
+function openNav() {
+    document.getElementById("side-nav").style.width = "250px";
+}
+
+function closeNav() {
+    document.getElementById("side-nav").style.width = "0px";
+}
